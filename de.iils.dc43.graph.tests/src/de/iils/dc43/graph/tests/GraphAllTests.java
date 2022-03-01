74d35e1b40c1bf872d0ea821d6b4c9ab8d61b5e1
DaliKallali <kallali.daly@gmail.com>
2022-03-01 23:00:24 +0100

    Add files via upload

---
+++ de.iils.dc43.graph.tests/src/de/iils/dc43/graph/tests/GraphAllTests.java
@@ -0,0 +1,46 @@
+/**
+ */
+package de.iils.dc43.graph.tests;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import junit.textui.TestRunner;
+
+/**
+ * <!-- begin-user-doc -->
+ * A test suite for the '<em><b>Graph</b></em>' model.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class GraphAllTests extends TestSuite {
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static void main(String[] args) {
+		TestRunner.run(suite());
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static Test suite() {
+		TestSuite suite = new GraphAllTests("Graph Tests");
+		return suite;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public GraphAllTests(String name) {
+		super(name);
+	}
+
+} //GraphAllTests
---
+++ de.iils.dc43.graph.tests/src/de/iils/dc43/graph/tests/GraphObjectTest.java
@@ -0,0 +1,54 @@
+/**
+ */
+package de.iils.dc43.graph.tests;
+
+import de.iils.dc43.graph.GraphObject;
+import junit.framework.TestCase;
+
+/**
+ * <!-- begin-user-doc -->
+ * A test case for the model object '<em><b>Object</b></em>'.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public abstract class GraphObjectTest extends TestCase {
+
+	/**
+	 * The fixture for this Object test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected GraphObject fixture = null;
+
+	/**
+	 * Constructs a new Object test case with the given name.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public GraphObjectTest(String name) {
+		super(name);
+	}
+
+	/**
+	 * Sets the fixture for this Object test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void setFixture(GraphObject fixture) {
+		this.fixture = fixture;
+	}
+
+	/**
+	 * Returns the fixture for this Object test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected GraphObject getFixture() {
+		return fixture;
+	}
+
+} //GraphObjectTest
